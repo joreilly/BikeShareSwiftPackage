7a2a8d3c0c5899c73cdbaea374bc22826704bce3
John O'Reilly <oreillyj@gmail.com>
2023-11-09 20:55:56 +0000

    swift package updates

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "BikeShareKit",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13),
+.macOS(.v12)
     ],
     products: [
         .library(
